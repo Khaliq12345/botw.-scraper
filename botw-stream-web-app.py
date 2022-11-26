aab27497b86e5959ba351b0fe408430b5e2c3d79
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2022-11-26 10:06:13 +0100

    Add files via upload

--- botw-stream-web-app.py
+++ botw-stream-web-app.py
@@ -1,69 +1,66 @@
 import os
 os.system("playwright install chromium")
-os.system('playwright install-deps')
 import streamlit as st
+import asyncio
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
 import pandas as pd
 from fake_useragent import UserAgent
 ua = UserAgent()
 
+
 company_list = []
 
-def start_scrape(category, pages):
+def scrape():
     with sync_playwright() as p:
-        browser = p.chromium.launch()
+        browser = p.chromium.launch(headless=False)
         page = browser.new_page(user_agent=ua.random)
-        for x in range(1, pages):
+        for x in range(1, int(pages)):
             print(f"page {x}")
-            try:
-                page.goto(f"https://botw.org/{category}/{pages}", wait_until= 'commit')
-                page.wait_for_selector('.d-md-block h5 > a')
-                html = page.inner_html('.description > .container')
-                soup = BeautifulSoup(html, 'html.parser')
-                cards = soup.select('.lock')
+            page.goto(f"https://botw.org/{category}/{x}", wait_until= 'commit')
+            page.wait_for_selector('.justify-content-between:nth-child(1) h5')
+            html = page.inner_html('.description > .container')
+            soup = BeautifulSoup(html, 'html.parser')
+            cards = soup.select('.lock')
 
-                for card in cards:
-                    try:
-                        name = card.select_one('.d-md-block h5 > a').text
-                    except:
-                        name = None
-                    try:
-                        address = card.select_one('strong').text
-                    except:
-                        address = None
-                    try:
-                        description = card.select_one('strong+ p').text
-                    except:
-                        description = None
-                    try:
-                        website = card.select_one('.listing_website_url a')['href']
-                    except:
-                        website = None
-                    try:
-                        phone = card.select_one('.ml-1').text
-                    except:
-                        phone = None
-                    try:
-                        star = card.select_one('.list-5 a').text
-                    except:
-                        star = None
+            for card in cards:
+                try:
+                    name = card.select_one('h5').text
+                except:
+                    name = None
+                try:
+                    address = card.select_one('strong').text
+                except:
+                    address = None
+                try:
+                    description = card.select_one('strong+ p').text
+                except:
+                    description = None
+                try:
+                    website = card.select_one('.listing_website_url a')['href']
+                except:
+                    website = None
+                try:
+                    phone = card.select_one('.ml-1').text
+                except:
+                    phone = None
+                try:
+                    star = card.select_one('.list-5 a').text
+                except:
+                    star = None
 
-                    company_info = {
-                        'Name': name,
-                        'Address': address,
-                        'Description': description,
-                        'Website': website,
-                        'Phone': phone,
-                        'Review star': star
-                    }
-                    company_list.append(company_info)
-            except:
-                pass
+                company_info = {
+                    'Name': name,
+                    'Address': address,
+                    'Description': description,
+                    'Website': website,
+                    'Phone': phone,
+                    'Review star': star
+                }
+                company_list.append(company_info)
             
 
         df = pd.DataFrame(company_list)
-        st.markdown('<h3>Data Frame of the scraped data</h3>', unsafe_allow_html=True)
         st.dataframe(df)
 
         csv = df.to_csv().encode('utf-8')
@@ -75,7 +72,7 @@ def start_scrape(category, pages):
         )
 
         browser.close()
-        
+
 if __name__ =='__main__':
     st.title('BOTW.ORG SCRAPER')
     st.caption('1. Note that all category to be input should be in lowercase')
@@ -90,6 +87,6 @@ if __name__ =='__main__':
         button = st.form_submit_button('scrape')
     if button:
         st.text('Scraping started')
-        start_scrape(category, int(pages))
+        scrape()
         st.balloons()
         st.success('Success')
